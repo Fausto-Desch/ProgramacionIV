13d38c4a3a45de01fa5fce4596a98557a8cdc1ed
JoacoBO <benamoortegajoaquin@gmail.com>
2025-09-07 20:50:25 -0300

    Ejercicio 1 entrega.

---
+++ TP1/Ejercicio 1/Ej1.ts
@@ -0,0 +1,18 @@
+interface Animal {
+  hacerSonido(): void;
+  moverse(): void;
+}
+
+class Perro implements Animal {
+  hacerSonido(): void {
+    console.log("Guau!");
+  }
+
+  moverse(): void {
+    console.log("El perro corre");
+  }
+}
+
+const miPerro = new Perro();
+miPerro.hacerSonido();
+miPerro.moverse();
